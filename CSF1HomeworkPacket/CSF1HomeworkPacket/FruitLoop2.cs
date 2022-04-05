0dc8aaf065114c48a70e25668cb4ae584128deff
John Siegel <jhsiegeljr@outlook.com>
2022-04-04 21:45:56 -0500

    added classes

---
+++ CSF1HomeworkPacket/CSF1HomeworkPacket/FruitLoop2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1HomeworkPacket
+{
+    class FruitLoop2
+    {
+        Console.WriteLine("FruitLoop2");
+    }
+}
---
+++ CSF1HomeworkPacket/CSF1HomeworkPacket/TemperstuureConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1HomeworkPacket
+{
+    class TemperstuureConverter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Temperature");
+        }
+    }
+}
